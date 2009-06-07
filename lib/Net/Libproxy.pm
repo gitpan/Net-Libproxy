56be88ac9609c4922f73103093c01e974b303209
Gonéri Le Bouder <goneri@lebouder.net>
2009-06-07 01:55:30 +0000

    Import of GONERI/Net-Libproxy-0.01 from CPAN.
    gitpan-cpan-distribution: Net-Libproxy
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         GONERI/Net-Libproxy-0.01.tar.gz
    gitpan-cpan-author:       GONERI
    gitpan-cpan-maturity:     released

---
+++ lib/Net/Libproxy.pm
@@ -0,0 +1,68 @@
+package Net::Libproxy;
+use 5.008000;
+use warnings;
+our $VERSION = '0.01';
+
+require Exporter;
+require DynaLoader;
+@ISA = qw(Exporter DynaLoader);
+@EXPORT = qw(proxy_factory_new proxy_factory_get_proxies);
+
+bootstrap Net::Libproxy;
+
+sub new {
+  my $self;
+
+  $self->{pf} = Net::Libproxy::proxy_factory_new();
+
+  bless $self;
+}
+
+sub getProxy {
+  my ($self, $url) = @_;
+
+  return Net::Libproxy::proxy_factory_get_proxies($self->{pf}, $url);
+}
+
+1;
+
+
+=head1 NAME
+
+Net::Libproxy - Perl binding for libproxy ( http://code.google.com/p/libproxy/  )
+
+=head1 SYNOPSIS
+
+  use Net::Libproxy;
+
+  $p = new Net::Libproxy;
+  $proxies = $p->getProxy('http://code.google.com/p/libproxy/');
+
+=head1 DESCRIPTION
+
+libproxy is a lightweight library which makes it easy to develop
+applications proxy-aware with a simple and stable API.
+
+=head2 EXPORT
+
+proxy_factory_new, proxy_factory_get_proxies, new and getProxy are exported.
+
+=head1 SEE ALSO
+
+Libproxy homepage: http://code.google.com/p/libproxy/
+
+
+=head1 AUTHOR
+
+Goneri Le Bouder, E<lt>goneri@rulezlan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 by Goneri Le Bouder
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
